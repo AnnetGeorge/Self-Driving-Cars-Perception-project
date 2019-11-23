8995470c4fd12d21a05d031da0e0d414e8052570
OSCAR <oidelima5031@gmail.com>
2019-11-22 21:44:45 -0400

    Shreishthas code

---
+++ data_builder.py
@@ -0,0 +1,89 @@
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+import csv
+import shutil
+
+def readCSV(filename):
+    paths = []
+    labels = []
+
+    with open(filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            paths.append(row[0])
+            labels.append(row[1])
+
+    return paths[1:], labels[1:]
+
+REBUILD_DATA = False
+
+class CarClassifier():
+
+    TRAINING_FOLDER_NAME = "trainval"
+    TEST_FOLDER_NAME = "test"
+    SAVE_SIZE = 500
+
+    training_data = []
+    testing_data = []
+    trainCount = 0
+    testCount = 0
+    numClasses = 4
+
+
+    #reading labels from csv file
+    paths, labels = readCSV('../labels.csv')
+    labels_dict = {}
+    for i in range(len(paths)):
+        labels_dict[paths[i]] = labels[i]
+
+
+    def make_training_data(self):
+        #BUILDING TRAINING DATA
+        for folder in tqdm(os.listdir(self.TRAINING_FOLDER_NAME)):
+            try:
+                #loading training data
+                for file in tqdm(os.listdir(os.path.join(self.TRAINING_FOLDER_NAME, folder))):
+                    if file.endswith('.jpg'):
+                        try:
+                            path = os.path.join(self.TRAINING_FOLDER_NAME, folder, file)
+                            label = int(self.labels_dict[os.path.join(folder, file)[:-10]])
+                            #dest = "image_" + str(self.trainCount) + ".jpg"
+                            dest = shutil.copyfile(path, os.path.join("train", str(label), "image_" + str(self.trainCount) + ".jpg"))
+                            self.trainCount += 1
+
+                        except Exception as e:
+                            pass
+
+            except Exception as e:
+                pass
+
+        print(self.trainCount)
+        for folder in tqdm(os.listdir(self.TEST_FOLDER_NAME)):
+            try:
+                #loading training data
+                for file in tqdm(os.listdir(os.path.join(self.TEST_FOLDER_NAME, folder))):
+
+                    if file.endswith('.jpg'):
+                        try:
+                            path = os.path.join(self.TEST_FOLDER_NAME, folder, file)
+                            dest = shutil.copyfile(path, os.path.join("val", str(0), "image_" + str(self.testCount) + ".jpg"))
+                            self.testCount += 1
+
+                        except Exception as e:
+                            pass
+
+            except Exception as e:
+                pass
+
+        #BUILDING TESTING DATA
+
+if REBUILD_DATA:
+    car_classifier= CarClassifier()
+    car_classifier.make_training_data()
+
+
+
+
+
